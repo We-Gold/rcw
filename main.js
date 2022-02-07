cba2a94a2cb1099b6ad8290816716af286974885
Weaver Goldman <we.goldm@gmail.com>
2022-02-06 21:10:57 -0500

    Added a basic assortment of 2d shapes.

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const CLEAR_COLOR = new rcw.Color(0,0,0,1)
 
 rcw.runLoop(() => {
 	c.clear(CLEAR_COLOR)
+
 	c.rect(c.width()/2 - 20, c.height()/2 - 20, 40, 40, new rcw.Color(150, 150, 0, 1))
+	c.rect_lines(c.width()/3 - 20, c.height()/3 - 20, 40, 40, 2, new rcw.Color(150, 150, 0, 1))
+
+	c.circle(c.width()/4, c.height()/2, 40, new rcw.Color(150, 150, 0, 1))
+	
+	c.ellipse(40, 40, 40, 50, 0, 0, Math.PI, new rcw.Color(150, 150, 0, 1))
+
+	c.line(c.width()/2, c.height()/2, c.width()/4, c.height()/2, 2, new rcw.Color(255, 255, 255, 1))
 })
