8b4496f2436bc9434e9e60844602271892325f79
Weaver Goldman <we.goldm@gmail.com>
2022-02-13 15:59:01 -0500

    Replaced classes with factories, and update the package version.

--- main.js
+++ main.js
@@ -7,18 +7,20 @@ const demo2dConfig = {
 
 const c = rcw.initCanvas(demo2dConfig)
 
-const CLEAR_COLOR = new rcw.Color(0,0,0,1)
+const CLEAR_COLOR = rcw.Color(0,0,0,1)
 
 rcw.runLoop(() => {
 	c.clear(CLEAR_COLOR)
 
 	const { mouseX, mouseY } = c.getMouse()
 
-	c.rect(c.width()/2 - 20, c.height()/2 - 20, 40, 40, new rcw.Color(150, 150, 0, 1))
-	c.rectLines(c.width()/3 - 20, c.height()/3 - 20, 40, 40, 8, new rcw.Color(150, 150, 0, 1))
+	c.rect(c.width()/2 - 20, c.height()/2 - 20, 40, 40, rcw.Color(150, 150, 0, 1))
+	c.rectLines(c.width()/3 - 20, c.height()/3 - 20, 40, 40, 8, rcw.Color(150, 150, 0, 1))
 
-	c.circle(mouseX, mouseY, 40, new rcw.Color(150, 150, 0, 1))
-	c.circleLines(mouseX, mouseY, 60, 10, new rcw.Color(150, 150, 0, 1))
+	c.circle(mouseX, mouseY, 40, rcw.Color(150, 150, 0, 1))
+	c.circleLines(mouseX, mouseY, 60, 10, rcw.Color(150, 150, 0, 1))
 	
-	c.line(c.width()/2, c.height()/2, mouseX, mouseY, 2, new rcw.Color(255, 255, 255, 1))
+	c.line(c.width()/2, c.height()/2, mouseX, mouseY, 2, rcw.Color(255, 255, 255, 1))
+
+	// c.text("Hi", 40, 40, rcw.Font("serif", 20), rcw.Color(255, 255, 255, 1))
 })
