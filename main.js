ed05b3d20a582918a102bea1d80b4be67c42de7e
Weaver Goldman <we.goldm@gmail.com>
2022-02-13 15:52:16 -0500

    Added basic shapes to the webgl library.

--- main.js
+++ main.js
@@ -2,28 +2,23 @@ import * as rcw from "./lib/main"
 
 const demo2dConfig = {
 	id: "demo2d", 
-	mode: rcw.CanvasMode.default, 
+	mode: rcw.CanvasMode.webgl, 
 }
 
 const c = rcw.initCanvas(demo2dConfig)
 
 const CLEAR_COLOR = new rcw.Color(0,0,0,1)
 
-let x = 40
-
 rcw.runLoop(() => {
-	const {mouse_x, mouse_y} = c.get_mouse()
-
 	c.clear(CLEAR_COLOR)
 
+	const { mouseX, mouseY } = c.getMouse()
+
 	c.rect(c.width()/2 - 20, c.height()/2 - 20, 40, 40, new rcw.Color(150, 150, 0, 1))
-	c.rect_lines(c.width()/3 - 20, c.height()/3 - 20, 40, 40, 2, new rcw.Color(150, 150, 0, 1))
+	c.rectLines(c.width()/3 - 20, c.height()/3 - 20, 40, 40, 8, new rcw.Color(150, 150, 0, 1))
 
-	c.circle(mouse_x, mouse_y, 40, new rcw.Color(150, 150, 0, 1))
+	c.circle(mouseX, mouseY, 40, new rcw.Color(150, 150, 0, 1))
+	c.circleLines(mouseX, mouseY, 60, 10, new rcw.Color(150, 150, 0, 1))
 	
-	c.ellipse(x, 40, 40, 50, 0, 0, Math.PI, new rcw.Color(150, 150, 0, 1))
-
-	c.line(c.width()/2, c.height()/2, mouse_x, mouse_y, 2, new rcw.Color(255, 255, 255, 1))
-
-	if (rcw.is_key_down("ArrowRight")) x += 1
+	c.line(c.width()/2, c.height()/2, mouseX, mouseY, 2, new rcw.Color(255, 255, 255, 1))
 })
